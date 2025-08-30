dfd291d7930dc27c964f2ed6e3f8868d6e09c8c7
Jatin Sharma <instinctultra13@gmail.com>
2025-08-30 22:19:18 +0530

    Update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Room from './pages/Room';
-import WatchTogether from './pages/watchTogether';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path='/watchTogether' element={<WatchTogether/>}/>
         <Route path="/room/:roomId" element={<Room />} />
       </Routes>
     </BrowserRouter>
